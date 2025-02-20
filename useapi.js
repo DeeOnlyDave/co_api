a1cb400eefc92f54e047f79f9c7a320ef707302e
DeeOnlyDave <88871199+DeeOnlyDave@users.noreply.github.com>
2025-02-19 21:25:30 -0500

    Added API

---
+++ useapi.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post("/items", (req, res) => {
+    data.push(req.body);
+    res.status(201).json({ message: "Item added", item: req.body });
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
